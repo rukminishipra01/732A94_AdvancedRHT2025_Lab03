adcd89796383485a8fd523183f669ff16eb87fb1
Cl4ryty <61427823+Cl4ryty@users.noreply.github.com>
2025-09-14 18:32:58 +0200

    task 1.2: add dataset; create documentation

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Wikipedia example graph
+#'
+#' A dataset describing a graph to use as example for Dijkstra's algorithm
+#'
+#' @format
+#' A data frame with 18 rows and 3 columns:
+#' \describe{
+#'   \item{v1}{Vertex 1 - from where the edge is outgoing}
+#'   \item{v2}{Vertex 2 - where the edge is incoming}
+#'   \item{w}{The weight of the edge}
+#' }
+#' @source <https://en.wikipedia.org/wiki/Euclidean_algorithm>
+"wiki_graph"
